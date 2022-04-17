fc32d3f0058e245f6c396ce7eeba159886784381
Stefan Filev <stefan.s.filev@gmail.com>
2022-04-17 11:31:10 +0100

    routes controller and view added with according style in scss

---
+++ app/controllers/questions_controller.rb
@@ -0,0 +1,26 @@
+class QuestionsController < ApplicationController
+  def ask
+  end
+
+  def answer
+    @answer = params[:question]
+    @coach_answer = if @answer.end_with?('?')
+                      'Silly question, get dressed and go to work!'
+                    elsif @answer == 'I am going to work'
+                      'Great!'
+                    else
+                      "I don't care, get dressed and go to work!"
+                    end
+  end
+
+  # def answer
+  #   @answer = params[:question]
+  #   if @answer.end_with?('?')
+  #     @coach_answer = 'Silly question, get dressed and go to work!'
+  #   elsif @answer == 'I am going to work'
+  #     @coach_answer = 'Great!'
+  #   else
+  #     @coach_answer = "I don't care, get dressed and go to work!"
+  #   end
+  # end
+end
